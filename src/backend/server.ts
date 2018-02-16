3cc2ca7def2029e4a445ed3920c5dbdb207a821b
rijine <e.rijin@gmail.com>
2018-02-17 00:29:16 +0530

    first server rendered html

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,1 +1,36 @@
-console.log('hi')
+import 'zone.js/dist/zone-node';
+
+const {
+  AppServerModuleNgFactory,
+  LAZY_MODULE_MAP
+} = require('../dist-server/main.bundle');
+//import { AppServerModuleNgFactory, LAZY_MODULE_MAP } from '../dist-server/main.bundle';
+import { renderModuleFactory } from '@angular/platform-server';
+
+import * as path from 'path';
+import * as fs from 'fs';
+import * as express from 'express';
+
+const app = express();
+
+const distPath = path.resolve(__dirname, '../dist');
+const indexFilePath = path.resolve(__dirname, '../dist/index.html');
+const indexHtml = fs.readFileSync(indexFilePath).toString();
+
+app.use(
+  express.static(distPath, {
+    index: false
+  })
+);
+
+app.get('/', async (req, res) => {
+  const html = await renderModuleFactory(AppServerModuleNgFactory, {
+    url: '/',
+    document: indexHtml
+  });
+  res.send(html);
+});
+
+app.listen(4000, () => {
+  console.log('http://localhost:4000/');
+});
